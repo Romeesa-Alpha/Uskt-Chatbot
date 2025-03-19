7c221dfe509e658b06e90329d47b42602cb7b6d4
Romeesa-Alpha <romeesaimran867@gmail.com>
2025-03-19 16:40:43 +0000

    Update Data

--- app.py
+++ app.py
@@ -324,7 +324,8 @@ def main():
 
 
     # pdf_file = ["./data/Uskt_Data.pdf", "./data/navigation.pdf", "./data/ReTrain_Data.pdf" , "./data/mozdoc.pdf"]
-    pdf_file = [ "./New_Data/new_locations.pdf " , "./New_Data/retrain.pdf "]
+
+    pdf_file = ["./new_data/data.pdf", "./new_data/moz.pdf", "./new_data/new_locations.pdf" , "./new_data/ReTrain.pdf"]
 
     if pdf_file:
         with st.spinner("Processing PDF..."):
